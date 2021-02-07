1086df84fd6287194d955301b4f9be9a5e3deb54
Giulia von Canal <giulivcanal@googlemail.com>
2021-02-07 17:05:40 +0100

    first commit

--- backgammon.py
+++ backgammon.py
@@ -16,6 +16,7 @@ Created on Sat Jan 30 22:12:48 2021
 # ============================================================================ #
 
 import random 
+import re
 
 
 def printBoard(board) :
@@ -39,9 +40,9 @@ def printBoard(board) :
                             
         elif i == 13 :
             
-            for j in range(13, 25) :
+            for j in range(24, 12, -1) :
                 
-                if j == 19 :
+                if j == 18 :
                     print("||\t||\t", end="")
                     
                 print(j, "\t", end="")
@@ -70,8 +71,8 @@ def printBoard(board) :
             print("\t" * 6, end="")
             
         elif i >= 8 : 
-            for point in range(13, 25) :
-                if point == 19: 
+            for point in range(24, 12, -1) :
+                if point == 18: 
                     print("||\t||\t", end="")
                 if not board[point]:
                     print("\t", end="")
@@ -92,58 +93,97 @@ def printBoard(board) :
     print("-" * 65)
     print("-" * 65)
         
-
-
     
 def rollDices() :
     
     dice1 = random.randint(1,6)
     dice2 = random.randint(1,6)
     
-    print("")
+    print("Dices rolling...\n")
     
     return [dice1, dice2]
 
-def moveChecker(board, color, start, dest) : 
-    
+
+def inputIsValid(inputStr) : 
+    if re.match('([1-9]|1[0-9]|2[0-4]) - ([1-9]|1[0-9]|2[0-4])', inputStr): 
+        return True
+    else : 
+        print("Input does not meet required format. Please try again!")
+        return False
+
+
+def moveIsPossible(board, color, start, dest) : 
+
+    if (color == 'b' and start > dest) or (color == 'w' and start < dest): 
+        print("You cannot move in this direction")
+        return False
+
     if board[start] and board[start][0] == color : 
-        board[start][1] -= 1
-        
-        if board[start][1] == 0 : 
-            board[start] = []
-            
+
         if not board[dest] : 
-            board[dest] = [color, 1]
-            
-            printBoard(board)
-            
-        elif board[dest][0] == color :
-            board[dest][1] += 1
-            
-            printBoard(board)
-        
-        else : 
+            return True 
+
+        else :
+
             if board[dest][1] > 1 : 
                 print("You cannot move your checker to", dest)
                 print("This point is occupied by the opponent")
-            
-            else : 
-                if color == 'b' :
-                    board[-1].append(['w', 1])
-                else : 
-                    board[-1].append(['b', 1])
-                
-                board[dest] = [color, 1]
-                
-                printBoard(board)
+
+                return False    
+            else :  
+                return True
                  
     else : 
         print("None of your checkers is on", start)
+        return False 
     
+
+
+def moveChecker(board, color, start, dest) : 
     
+    board[start][1] -= 1
+        
+    if board[start][1] == 0 : 
+        board[start] = []
+            
+    if not board[dest] : 
+        board[dest] = [color, 1]
+                 
+    elif board[dest][0] == color :
+        board[dest][1] += 1
+        
+    else : 
+
+        if color == 'b' :
+            board[-1].append(['w', 1])
+        else : 
+            board[-1].append(['b', 1])
+                
+            board[dest] = [color, 1]
+
+    printBoard(board)
+                
+
+
+def makeMove(color) : 
+
+    if color == 'b' : 
+        print("Black's turn.")
+    else : 
+        print("White's turn.")
     
+    result = rollDices()
+    print(result[0], result[1]) 
     
-                         
+    inputStr = input("\nEnter move:\t")
+    inputList = inputStr.split(" - ")
+
+    while not inputIsValid(inputStr) or not moveIsPossible(board, color, int(inputList[0]), int(inputList[1])) : 
+        inputStr = input("\nEnter move:\t")
+        inputList = inputStr.split(" - ")
+    
+    moveChecker(board, color, int(inputList[0]), int(inputList[1])) 
+                       
     
        
 ###################################
@@ -151,13 +191,11 @@ def moveChecker(board, color, start, dest) :
 
 board = {1: ['b', 2], 2: [], 3: [], 4: [], 5: [], 6: ['w', 2],
             7: [], 8: ['w', 3], 9: [],  10: [], 11 : [], 12: ['b',5],
-            13: ['w', 2], 14: [], 15: [], 16: [], 17: [], 18: ['b', 5],
-            19: [], 20: ['b', 3], 21: [], 22: [], 23: [], 24: ['w', 5], -1 : []}
+            13: ['w', 5], 14: [], 15: [], 16: [], 17: ['b', 3], 18: [],
+            19: ['b', 5], 20: [], 21: [], 22: [], 23: [], 24: ['w', 2], -1 : []}
 
 
 printBoard(board)
-
-moveChecker(board, 'b', 1, 6)
 
 ## game loop 
 
@@ -165,18 +203,7 @@ running = True
 
 while running : 
     
-    print("Black's turn.")
-    
-    result = rollDices()
-    print(result[0], result[1]) 
-    
-    #moveInput = input()
+    makeMove('b')
+    makeMove('w')
 
-    #test2
-
-
-
-
-
-
-#testtest
+    running = False
